543400c7f4cd894f38f3b1fb0fc4239e8191360b
Jacob Samro <admin@jsam.in>
2017-03-18 20:53:35 +0530

    Simple Service Code Added

--- app/src/main/java/in/a6un/sqliteviewer/sample/StartActivity.java
+++ app/src/main/java/in/a6un/sqliteviewer/sample/StartActivity.java
@@ -1,17 +1,20 @@
 package in.a6un.sqliteviewer.sample;
 
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-import java.util.ArrayList;
+import org.json.JSONException;
 
-import a6un.sqliteviewer.CreateTestDatabase;
+import a6un.sqliteviewer.Databases;
+import a6un.sqliteviewer.SQLiteDataHelper;
 import a6un.sqliteviewer.SQLiteService;
 
 public class StartActivity extends AppCompatActivity {
 
-    ArrayList<String> databases = new ArrayList<String>();
+    Databases databases = new Databases();
 
 
     @Override
@@ -19,13 +22,32 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        CreateTestDatabase createTestDatabase = new CreateTestDatabase(getApplicationContext());
 
-        databases.add("test");
+       //Creating a SQLite Helper
 
-        SQLiteService sqLiteService = new SQLiteService(getApplicationContext(),databases);
+        SQLiteDataHelper dbHelper = new SQLiteDataHelper(this,"test",1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
 
 
+        // Creating a Test Database
+
+        SQLiteDataHelper.createTestDB(db);
+
+
+        try {
+
+            databases.add("test",1);
+
+
+            SQLiteService sqLiteService = new SQLiteService(this,databases.getDatabases());
+
+            sqLiteService.initiate("http://192.168.1.102:8000");
+
+            //Log.d("TABLE DATA :", sqLiteService.getData("test","posts",0).toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
